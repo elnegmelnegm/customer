06267fda5542382e5c764ab1ccd44d600c1e46b8
elnegmelnegm <48047676+elnegmelnegm@users.noreply.github.com>
2024-01-13 07:28:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # Display header
 st.set_page_config(
-    page_title="ُEDA AI Chat",
+    page_title="ُEDA centre",
     page_icon="https://www.edaegypt.gov.eg/media/wc3lsydo/group-287.png",
     layout="wide",
 )
@@ -11,7 +11,7 @@ st.set_page_config(
 
 st.markdown('''
 <img src="https://www.edaegypt.gov.eg/media/wc3lsydo/group-287.png" width="250" height="100">''', unsafe_allow_html=True)
-st.title ("EDA trusted Sources")
+st.title ("EDA trusted Center")
 st.markdown('''
     تحتوي على مصادر موثوقه من هيئة الدواء المصرية ''', unsafe_allow_html=True)
 
@@ -19,9 +19,15 @@ st.link_button("للاستفسار عن معلومات دوائية موثوقة
 st.link_button("الاستفسار عن توافر المستحضرات الدوائية", "https://edaegypt.gov.eg/ar/%D8%A7%D9%84%D8%AE%D8%AF%D9%85%D8%A7%D8%AA/%D8%A7%D9%84%D8%A7%D8%B3%D8%AA%D9%81%D8%B3%D8%A7%D8%B1-%D8%B9%D9%86-%D8%AA%D9%88%D8%A7%D9%81%D8%B1-%D8%A7%D9%84%D9%85%D8%B3%D8%AA%D8%AD%D8%B6%D8%B1%D8%A7%D8%AA-%D8%A7%D9%84%D8%AF%D9%88%D8%A7%D8%A6%D9%8A%D8%A9/", use_container_width= True, help="تتيح الخدمة لكافة المواطنين الاستفسار عن مدى توافر المستحضرات الدوائية في السوق الدوائي المصري، ومن ثم تقوم الإدارة المعنية بهيئة الدواء المصرية بالتواصل مع مقدم الاستفسار للإجابة على الاستفسار المقدم، وكذلك يمكنكم التواصل معنا مباشرة على الخط الساخن 15301")
 
 st.link_button("لمتابعة المنشورات الصادرة من هيئة الدواء", "https://app.powerbi.com/view?r=eyJrIjoiZDNmZDU0NDEtZmE0ZC00NmQyLTk5MWItNjNjMGY4NTgyOGEzIiwidCI6ImUxNjE5YjA3LTAxMjAtNGRmOS04NjZkLTNhNjA0MDVjMmMxNCJ9&pageName=ReportSection", use_container_width= True,help="تتيح هذه الخدمة للمواطن إمكانية تتبع المنشورات الصادرة من هيئة الدواء المصرية التي تصدر بشكل دوري ")
+st.link_button("للإبلاغ عن المواد الدعائية الدوائية الغير ملائمة ", "https://edaegypt.gov.eg/ar/%D8%A7%D9%84%D8%AE%D8%AF%D9%85%D8%A7%D8%AA/%D8%A7%D9%84%D8%A5%D8%A8%D9%84%D8%A7%D8%BA-%D8%B9%D9%86-%D9%85%D9%88%D8%A7%D8%AF-%D8%AF%D8%B9%D8%A7%D8%A6%D9%8A%D8%A9-%D8%AF%D9%88%D8%A7%D8%A6%D9%8A%D8%A9-%D8%BA%D9%8A%D8%B1-%D9%85%D9%84%D8%A7%D8%A6%D9%85%D8%A9/", use_container_width= True,help="تتيح هذه الخدمة للمواطنين إمكانية الإبلاغ عن المخالفات المتعلقة بمواد التسويق والإعلان الدوائي ")
+
+st.link_button("الإبلاغ عن الآثار الجانبية للمستحضرات والمستلزمات الطبية", "https://edaegypt.gov.eg/ar/%D8%A7%D9%84%D8%AE%D8%AF%D9%85%D8%A7%D8%AA/%D8%A7%D9%84%D8%A5%D8%A8%D9%84%D8%A7%D8%BA-%D8%B9%D9%86-%D8%A7%D9%84%D8%A2%D8%AB%D8%A7%D8%B1-%D8%A7%D9%84%D8%AC%D8%A7%D9%86%D8%A8%D9%8A%D8%A9/", use_container_width= True,help=" تتيح هذه الخدمة للمواطن إمكانية الإبلاغ عن الآثار الجانبية للأدوية أو المستحضرات الحيوية واللقاحات أو مستحضرات التجميل أو المستلزمات الطبية ")
+
+st.link_button("للإبلاغ عن الأدوية المهربة / الغير مسجلة / المغشوشة / المنتهية الصلاحية", "https://edaegypt.gov.eg/ar/%D8%A7%D9%84%D8%AE%D8%AF%D9%85%D8%A7%D8%AA/%D8%A7%D9%84%D8%A5%D8%A8%D9%84%D8%A7%D8%BA-%D8%B9%D9%86-%D8%A7%D9%84%D8%A2%D8%AB%D8%A7%D8%B1-%D8%A7%D9%84%D8%AC%D8%A7%D9%86%D8%A8%D9%8A%D8%A9/", use_container_width= True,help="تتيح هذه الخدمة للمواطن إمكانية الإبلاغ عن الآثار الجانبية للأدوية أو المستحضرات الحيوية واللقاحات أو مستحضرات التجميل أو المستلزمات الطبية")
 
 
-st.title ("EDA AI Sources")
+
+st.title ("EDA AI Center")
 st.markdown('''
 Powered by Google AI <img src="https://seeklogo.com/images/G/google-ai-logo-996E85F6FD-seeklogo.com.png" width="20" height="20"> Streamlit <img src="https://global.discourse-cdn.com/business7/uploads/streamlit/original/2X/f/f0d0d26db1f2d99da8472951c60e5a1b782eb6fe.png" width="22" height="22"> Python <img src="https://i.ibb.co/wwCs096/nn-1-removebg-preview-removebg-preview.png" width="22" height="22">''', unsafe_allow_html=True)
 st.markdown('''
